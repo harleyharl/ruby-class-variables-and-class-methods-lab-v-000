c117bb40aedcf323a3d2ceb4dd7f74809fa77273
Harley Rayner <harleymteden@gmail.com>
2018-02-16 21:23:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,9 +44,8 @@ class Song
     @@artists.each do |artist|
       if artist_count_hash.include?(artist)
         artist_count_hash[artist] += 1
-        binding.pry
       else
-        artist_count_hash = 1
+        artist_count_hash[artist] = 1
       end
     end
     artist_count_hash
